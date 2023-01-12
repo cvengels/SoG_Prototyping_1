429fbadc4cb7cab66b70462ffc58c9848b075455
C.Vengels <6281096+cvengels@users.noreply.github.com>
2023-01-12 06:23:59 +0100

    Animators started, cat working (kinda)

--- Assets/Scripts/PlayerMovementTest.cs
+++ Assets/Scripts/PlayerMovementTest.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 public class PlayerMovementTest : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D playerRB;
-    [SerializeField] private InputManager inputManager;
+
+    [SerializeField] private Animator animator;
     
     [SerializeField] private string playerName;
     [SerializeField] private float movementSpeed = 5f;
@@ -12,25 +13,52 @@ public class PlayerMovementTest : MonoBehaviour
     void Start()
     {
         playerRB = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
     
     void Update()
     {
         if (playerName == "PlayerOne")
         {
+            // left
             if (Input.GetKey(KeyCode.A))
             {
                 playerRB.velocity = new Vector2(-1 * movementSpeed, playerRB.velocity.y);
+                transform.localScale = new Vector2(-1, 1);
             }
+            // right
             else if (Input.GetKey(KeyCode.D))
             {
                 playerRB.velocity = new Vector2(1 * movementSpeed, playerRB.velocity.y);
+                transform.localScale = new Vector2(1, 1);
             }
-            
+
+            // jump (TODO change to multi-purpose button)
             if (Input.GetKey(KeyCode.E))
             {
                 playerRB.velocity = new Vector2(playerRB.velocity.x, jumpHeight);
             }
+            
+            // Animations
+            if (Mathf.Abs(playerRB.velocity.x) < 0.01f && Mathf.Abs(playerRB.velocity.y) < 0.01f)
+            {
+                animator.SetBool("catWalk", false);
+                animator.SetBool("catJump", false);
+            }
+            else
+            {
+                if (Mathf.Abs(playerRB.velocity.y) > 0.05f)
+                {
+                    animator.SetBool("catJump", true);
+                    animator.SetBool("catWalk", false);
+                }
+                else
+                {
+                    animator.SetBool("catWalk", true);
+                    animator.SetBool("catJump", false);
+                }
+            }
+            
             
         }
         
@@ -39,10 +67,12 @@ public class PlayerMovementTest : MonoBehaviour
             if (Input.GetKey(KeyCode.LeftArrow))
             {
                 playerRB.velocity = new Vector2(-1 * movementSpeed, playerRB.velocity.y);
+                transform.localScale = new Vector2(-1, 1);
             }
             else if (Input.GetKey(KeyCode.RightArrow))
             {
                 playerRB.velocity = new Vector2(1 * movementSpeed, playerRB.velocity.y);
+                transform.localScale = new Vector2(1, 1);
             }
             
             if (Input.GetKey(KeyCode.Space))
